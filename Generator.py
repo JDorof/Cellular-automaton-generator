4539c2bcede69ce6199b8824da78e8dadb41405d
JDorof <demadorofeevicok977@gmail.com>
2024-09-03 13:04:05 +0300

    merge blur_types, gradients, neighborhoods, changeing save function

--- Generator.py
+++ Generator.py
@@ -1,5 +1,4 @@
 import rules
-import blur_types
 
 import numpy as np
 import scipy.signal
@@ -11,6 +10,145 @@ from random import randint, seed
 
 seed(a=rules.seed, version=2)
 np.random.seed(seed=randint(0, 2^32 - 1))
+
+
+class NeighborhoodClass:
+    standart3x3 = np.ones((3, 3), dtype=int)
+
+    moore_neighborhood_1order = np.ones((3, 3), dtype=int)
+    moore_neighborhood_1order[1, 1] = 0
+
+    moore_neighborhood_2order = np.ones((5, 5), dtype=int)
+    moore_neighborhood_2order[2, 2] = 0
+
+    plus = np.ones((3, 3), dtype=int)
+    plus[0][0] = plus[0][2] = plus[2][0] = plus[2][2] = 0
+
+    cross = np.ones((3, 3), dtype=int)
+    cross[0][1] = cross[1][0] = cross[1][2] = cross[2][1] = 0
+
+    horizontal = np.ones((3, 3), dtype=int)
+    horizontal[1][0] = horizontal[1][2] = 1
+
+    vertical = np.ones((3, 3), dtype=int)
+    vertical[0][1] = vertical[2][1] = 1
+
+
+class BlurClass:
+    # сумма всех элементов должа быть равна 1
+    # размер матрицы оставляем 3 на 3, так как в функции Blur при вычислении используется поле 3 на 3
+
+    standart3x3 = np.array([
+        [0.0625, 0.125, 0.0625],
+        [0.125, 0.25, 0.125],
+        [0.0625, 0.125, 0.0625]
+    ])
+
+    standart5x5 = np.array([
+        [0.00296902, 0.01330621, 0.02193823, 0.01330621, 0.00296902],
+        [0.01330621, 0.0596343,  0.09832033, 0.0596343,  0.01330621],
+        [0.02193823, 0.09832033, 0.16210282, 0.09832033, 0.02193823],
+        [0.01330621, 0.0596343,  0.09832033, 0.0596343,  0.01330621],
+        [0.00296902, 0.01330621, 0.02193823, 0.01330621, 0.00296902]
+    ])
+
+    outside = np.array([
+        [0.125, 0.125, 0.125],
+        [0.125, 0.0, 0.125],
+        [0.125, 0.125, 0.125]
+    ])
+
+    square = np.array([
+            [0.166666, 0.166666, 0.166666],
+            [0.166666, 0.0, 0.166666],
+            [0.166666, 0.166666, 0.166666]
+        ])
+
+    cross = np.array([
+        [0.15, 0.0, 0.15],
+        [0.0, 0.4, 0.0],
+        [0.15, 0.0, 0.15]
+    ])
+
+    plus = np.array([
+        [0.0, 0.15, 0.0],
+        [0.15, 0.4, 0.15],
+        [0.0, 0.15, 0.0]
+    ])
+
+
+    outside_standart = np.array([
+        [0.083333, 0.166666, 0.083333],
+        [0.166666, 0.0, 0.166666],
+        [0.083333, 0.166666, 0.083333]
+    ])
+
+    outside_cross = np.array([
+        [0.25, 0.0, 0.25],
+        [0.0, 0.0, 0.0],
+        [0.25, 0.0, 0.25]
+    ])
+
+    outside_plus = np.array([
+        [0.0, 0.25, 0.0],
+        [0.25, 0.0, 0.25],
+        [0.0, 0.25, 0.0]
+    ])
+
+    horizontal = np.array([
+        [0.0, 0.0, 0.0],
+        [0.25, 0.5, 0.25],
+        [0.0, 0.0, 0.0]
+    ])
+
+    vertical = np.array([
+        [0.0, 0.25, 0.0],
+        [0.0, 0.5, 0.0],
+        [0.0, 0.25, 0.0]
+    ])
+
+    horizontal_cross = np.array([
+        [0.1, 0.0, 0.1],
+        [0.15, 0.3, 0.15],
+        [0.1, 0.0, 0.1]
+    ])
+
+    vertical_cross = np.array([
+        [0.1, 0.15, 0.1],
+        [0.0, 0.3, 0.0],
+        [0.1, 0.15, 0.1]
+    ])
+
+    down = np.array([
+        [0.15, 0.2, 0.15],
+        [0.0, 0.4, 0.0],
+        [0.0, 0.0, 0.0]
+    ])
+
+
+class GradientClass:
+
+    black_to_white = [(0, 0, 0), (28, 28, 28), (56, 56, 56), (85, 85, 85), (113, 113, 113), (141, 141, 141), (170, 170, 170), (198, 198, 198), (226, 226, 226), (255, 255, 255)]
+    ocean_beach_forest = [(5, 6, 27), (11, 15, 134), (25, 68, 178), (60, 143, 215), (60, 208, 215), (237, 255, 68), (79, 255, 0), (70, 200, 11), (29, 145, 32), (9, 110, 12)]
+    black_orange_yellow_white = [(0, 0, 0, 255), (63, 27, 0, 255), (127, 54, 0, 255), (191, 81, 0, 255), (255, 108, 0, 255), (255, 157, 0, 255), (255, 206, 0, 255), (255, 255, 0, 255), (255, 255, 127, 255), (255, 255, 255, 255)]
+    dark_grey_brown = [(42, 39, 30), (48, 46, 39), (55, 53, 48), (67, 65, 59), (79, 77, 71), (91, 89, 83), (103, 101, 95), (116, 113, 107), (91, 89, 83), (55, 53, 48)]
+    grass = [(21, 21, 21), (23, 27, 24), (26, 33, 27), (28, 40, 31), (31, 46, 34), (34, 53, 38), (42, 69, 47), (50, 85, 57), (59, 102, 67), (69, 119, 77)]
+
+
+    def ReadGradient(path):
+        to_read = Image.open(path)
+        gradient = [to_read.getpixel((x, 0)) for x in range(0, 10)]
+        return gradient
+
+
+    def SaveGradient(gradient, name):
+        to_save = Image.new('RGBA', (10, 1))
+        to_save.putdata(gradient)
+        to_save.save(f'gradients/{name}')
+
+
+def InitializeField(chances, shape):
+    return np.random.choice(chances, size=shape).astype("int32")
 
 
 def ReplaceCells(field: np.ndarray, replace: list, to: list, p: float = 1) -> np.ndarray:
@@ -81,10 +219,6 @@ def Blur(field: np.ndarray, blur_type: np.ndarray, target_values: list = range(1
         field = blurred_field
 
     return np.round(field).astype(int)
-
-
-def InitializeField(chances, shape):
-    return np.random.choice(chances, size=shape).astype("int32")
 
 
 def UpdateField(
@@ -188,10 +322,11 @@ def RunAutomaton(
 
     return grid
 
-def SaveFromNdarray(field: np.ndarray, local_time):
-    im = Image.new('RGB', (rules.width, rules.height))
-    result = list(field.reshape(rules.height*rules.width))
-    for i in range(rules.width * rules.height):
-        result[i] = rules.gradient[result[i] - 1]
+
+def SaveImageFromNdarrayMatrix(field: np.ndarray, path: str, gradient: list, sizes: tuple):
+    im = Image.new('RGB', sizes)
+    result = list(field.reshape(sizes[0] * sizes[1]))
+    for i in range(sizes[0] * sizes[1]):
+        result[i] = gradient[result[i] - 1]
     im.putdata(result)
-    im.save(f'results/{local_time}/picture.png')
+    im.save(path)
--- rules.py
+++ rules.py
@@ -1,4 +1,4 @@
-import gradients
+import Generator
 import numpy as np
 import time
 
@@ -19,27 +19,3 @@ width = 200
 boundary = "wrap"
 # boundary = "fill"
 # boundary = "symm"
-
-'''Colors'''
-gradient = gradients.black_orange_yellow_white
-# gradient = [x for x in reversed(gradients.ocean_beach_forest)]
-
-
-'''Neighborhoods'''
-standart3x3 = np.ones((3, 3), dtype=int)
-
-moore_neighborhood_1order = np.ones((3, 3), dtype=int)
-moore_neighborhood_1order[1, 1] = 0
-
-moore_neighborhood_2order = np.ones((5, 5), dtype=int)
-moore_neighborhood_2order[2, 2] = 0
-
-plus = np.ones((3, 3), dtype=int)
-plus[0][0] = plus[0][2] = plus[2][0] = plus[2][2] = 0
-
-cross = np.ones((3, 3), dtype=int)
-cross[0][1] = cross[1][0] = cross[1][2] = cross[2][1] = 0
-
-# horizontal = [[0, -1], [0, 1]]
-# vertical = [[-1, 0], [1, 0]]
-
